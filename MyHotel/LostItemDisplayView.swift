4240da7198628c9858f73db6109d1ddeb431d213
YutKigo <ykigoshi0829@gmail.com>
2024-10-31 02:15:27 +0900

    Lost item register function was added

---
+++ MyHotel/LostItemDisplayView.swift
@@ -0,0 +1,18 @@
+//
+//  LostItemDisplayView.swift
+//  MyHotel
+//
+//  Created by 木越湧太 on 2024/10/31.
+//
+
+import SwiftUI
+
+struct LostItemDisplayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LostItemDisplayView()
+}
